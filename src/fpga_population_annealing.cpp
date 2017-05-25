7afd6a52ccfa71675d9db32b833e5530a18ee0d3
Chris Pattison <chpattison@gmail.com>
2017-05-24 22:05:05 -0500

    Dirty FPGA implementation. Missing initalization.

--- src/fpga_population_annealing.cpp
+++ src/fpga_population_annealing.cpp
@@ -7,56 +7,162 @@
 
 namespace propane
 {
-FpgaPopulationAnnealing::Resample(double new_beta, double population_fraction) {
-    // TODO: add in support for padding on the end
-    
-    std::vector<double> energy(population_.size() * 64);
-    std::fill(energy.begin(), energy.end(), 0.0);
 
-    // Compute energy of each replica
-    for(int k = 0; k < structure_.Adjacent().outerSize(); ++k) {
-        for(Eigen::SparseTriangularView<Eigen::SparseMatrix<EdgeType>,Eigen::Upper>::InnerIterator 
-            it(structure_.Adjacent().triangularView<Eigen::Upper>(), k); it; ++it) {
-            for(i = 0; i < population_.begin().size(); ++i) {
-                for(int j = 0; j < 64; ++j) {
-                    index = i*64 + j;
-                    energy[index] += 
-                        ((population_[k][i] >> j) | 0x01 ? 1 : -1)
-                        * ((population_[it.index()][i] >> j) | 0x01 ? 1 : -1)
-                        * it.value();
-                }
-            }
+std::vector<FpgaPopulationAnnealing::Result> FpgaPopulationAnnealing::Run() {
+    std::vector<Result> results;
+
+    replicas_.resize(average_population_);
+    replica_families_.resize(average_population_);
+    
+    for(auto& r : replicas_) {
+        r = StateVector();
+        r.resize(structure_.size());
+        for(std::size_t k = 0; k < r.size(); ++k) {
+            r(k) = rng_.Probability() < 0.5 ? 1 : -1;
         }
     }
 
-    // Compute resampling weights (suitability)
-    std::vector<double> weights(energy.size());
-    std::transform(energy.begin(), energy.end(), weights.begin(), [&](double energy) 
-        {return std::exp(-(new_beta-beta_) * energy);});
+    std::iota(replica_families_.begin(), replica_families_.end(), 0);
+    beta_ = schedule_.front().beta;
+    std::vector<double> energy;
 
-    double summed_weights = std::accumulate(weights.begin(), weights.end(), 0.0);
-    double normalize = average_population_ / summed_weights;
+    for(auto step : schedule_) {
+        Result observables;
 
-    // Determine number of copies of each replica
-    std::vector<int> num_replicas(energy.size());
-    std::transform(weights.begin(), weights.end(), num_replicas.begin(), [&](double weight) 
-        {return weight - std::floor(weight)) > rng_.Probability() ? std::ceil(weight) : std::floor(weight);});
+        auto time_start = std::chrono::high_resolution_clock::now();
+        if(step.beta != beta_) {
+            observables.norm_factor = Resample(step.beta, step.population_fraction);
+        }
+        for(std::size_t k = 0; k < replicas_.size(); ++k) {
+            SweepPopulation(step.sweeps);
+        }
+        observables.montecarlo_walltime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
 
-    // Copy spins to new vector, copy families to new vector
-    int new_size = std::accumulate(num_replicas.begin(), num_replicas.end(), 0);
-    // Number of uint64_t required to store packed spins
-    int packed_size = new_size/64 + (new_size % 64 != 0);
-    // Make allocations
-    StateVectorPack new_population(population_.size());
-    std::for_each(new_population.begin(), new_population.end(), [&](auto& v) { v.resize(packed_size); });
-    std::vector<int> new_families(new_size);
+        energy.resize(replicas_.size());
+        for(std::size_t k = 0; k < replicas_.size(); ++k) {
+            energy[k] = Hamiltonian(replicas_[k]);
+        }
+        Eigen::Map<Eigen::VectorXd> energy_map(energy.data(), energy.size());
+        // Basic observables
+        observables.beta = beta_;
+        observables.population = replicas_.size();
+        observables.average_energy = energy_map.mean();
+        observables.average_squared_energy = energy_map.array().pow(2).mean();
+        observables.ground_energy = energy_map.minCoeff();
+        // Round-off /probably/ isn't an issue here
+        observables.grounded_replicas = energy_map.array().unaryExpr(
+            [&](double E){return E == observables.ground_energy ? 1 : 0;}).sum();
+        // Family statistics
+        std::vector<double> family_size = FamilyCount();
+        std::transform(family_size.begin(),family_size.end(),family_size.begin(),
+            [&](double n) -> double {return n /= observables.population;});
+        // Entropy
+        observables.entropy = -std::accumulate(family_size.begin(), family_size.end(), 0.0, 
+            [](double acc, double n) {return acc + n*std::log(n); });
+        // Mean Square Family Size
+        observables.mean_square_family_size = observables.population * 
+            std::accumulate(family_size.begin(), family_size.end(), 0.0, [](double acc, double n) {return acc + n*n; });
+        if(step.energy_dist) {
+            // Energy
+            observables.energy_distribution = BuildHistogram(energy);
+        }
+        
+        if(step.overlap_dist) {
+            // Overlap
+            std::vector<std::pair<int, int>> overlap_pairs = BuildReplicaPairs();
+            std::vector<double> overlap_samples(overlap_pairs.size());
+>
+            std::transform(overlap_pairs.begin(), overlap_pairs.end(), overlap_samples.begin(),
+                [&](std::pair<int, int> p){return Overlap(replicas_[p.first], replicas_[p.second]);});
+            observables.overlap = BuildHistogram(overlap_samples);
+            // Link Overlap
+            std::transform(overlap_pairs.begin(), overlap_pairs.end(), overlap_samples.begin(),
+                [&](std::pair<int, int> p){return LinkOverlap(replicas_[p.first], replicas_[p.second]);});
+            observables.link_overlap = BuildHistogram(overlap_samples);
+        }
 
-    // Make copies
-    for(i = 0; i < new_population.size(); ++i) {
-        for(j = 0; j < packed_size; ++j) {
-            std::uint64_t new_pack
-            for(k = 0; k < 64; ++k) {
+        observables.seed = rng_.GetSeed();
+        observables.sweeps = step.sweeps;
+        results.push_back(observables);
+    }
+    return results;
+}
 
+FpgaPopulationAnnealing::FpgaPopulationAnnealing(Graph& structure, Config config) {
+    // mmap bars
+    cfg_disc_ = open("/sys/bus/pci/devices/0000:81:00.0/resource0", O_RDWR | O_SYNC);
+    cfg_bar_ = reinterpret_cast<volatile std::std::uint32_t*>(mmap(nullptr, , PROT_READ | PROT_WRITE, MAP_SHARED, cfg_disc_, 0));
+    if(!cfg_bar_) {
+        throw std::exception("mmap failed.");
+    }
+    
+    mdl_disc_ = open("/sys/bus/pci/devices/0000:81:00.0/resource0", O_RDWR | O_SYNC);
+    mdl_bar_ = reinterpret_cast<volatile std::std::uint32_t*>(mmap(nullptr, , PROT_READ | PROT_WRITE, MAP_SHARED, mdl_disc_, 0));
+    if(!mdl_bar_) {
+        throw std::exception("mmap failed.");
+    }
+
+    mem_disc = open("/sys/bus/pci/devices/0000:81:00.0/resource0", O_RDWR | O_SYNC);
+    mem_bar_ = reinterpret_cast<volatile std::std::uint32_t*>(mmap(nullptr, , PROT_READ | PROT_WRITE, MAP_SHARED, mem_disc_, 0));
+    if(!mem_bar_) {
+        throw std::exception("mmap failed.");
+    }
+    // load configuration
+
+}
+
+FpgaPopulationAnnealing::SweepPopulation(int sweeps) {
+    int base_pointer = 0;
+
+    for(int k = 0; k <= replicas_.size() / 64; k++) {
+        std::vector<std::uint64_t> replica_pack(replicas_.front().size());
+        // Package replicas
+        for(int i = 0; i < replica_pack.size(); ++i) {
+            replica_pack[i] = 0;
+            // Assemble single package
+            for(int j = 0; j < 64; ++j) {
+                replica_pack[j] <<= 1;
+                if(j + k*64 < replicas.size()) {
+                    replica_pack[j] |= replicas_[k*64+j](i) == 1 ? 1 : 0;
+                }
+            }
+        }
+
+        // Copy to accelerator memory
+        for(int i = 0; i < replica_pack.size(); ++i) {
+            mem_bar_[base_pointer + i + replica_pack.size() * k] = replica_pack[i]; 
+        }
+    }
+
+// -------
+
+    // Configure and run
+    cfg_bar_[kReplicaSetCount] = (replicas_.size()+63) / 64;
+    cfg_bar_[kBasePointer] = base_pointer;
+    cfg_bar_[kBeta] = beta_;
+    cfg_bar_[kSweeps] = sweeps;
+
+    // Wait for completion
+    while(cfg_bar_[kSweeps] > 0) { ; }
+
+// -------
+
+    // Unpack
+    for(int k = 0; k <= replicas_.size() / 64; k++) {
+        std::vector<std::uint64_t> replica_pack(replicas_.front().size());
+        
+        // Copy froms accelerator memory
+        for(int i = 0; i < replica_pack.size(); ++i) {
+            replica_pack[i] = mem_bar_[base_pointer + i + replica_pack.size() * k];
+        }
+        
+        // Unpack replicas
+        for(int i = replica_pack.size()-1; i >= 0; --i) {
+            for(int j = 0; j < 64; ++j) {
+                replica_pack[j] >>= 1;
+                if(j + k*64 < replicas.size()) {
+                    replicas_[k*64+j](i) = replica_pack[j] & 1 ? 1 : -1;
+                }
             }
         }
     }
--- src/fpga_population_annealing.hpp
+++ src/fpga_population_annealing.hpp
@@ -11,15 +11,30 @@ namespace propane
  * This implementation stores spin data very differently than the others due to the need to offload to the FPGA
  * population_ has dimensions num(spins) x num(replicas/64)
  */
-class FpgaPopulationAnnealing : public PopulationAnnealingBase {
-    MonteCarloDriver driver_;
-public:
+class FpgaPopulationAnnealing : public PopulationAnnealing {
+private:
+    const int kVendorID = 0x7075;
+    const int kDeviceID = 0x1337;
+
+    const std::size_t kMemSize = 0x200'000;
+
+    const int kPackSize = 256;
+
+    volatile std::uint32_t* cfg_bar_;
+    volatile std::uint32_t* mdl_bar_;
+    volatile std::uint64_t* mem_bar_;
+
+    const int kReplicaSetCount = 0;
+    const int kSpinCount = 1;
+    const int kBasePointer = 2;
+    const int kSeed = 3;
+    const int kSweeps = 4;
+    const int kBeta = 5;
 protected:
+    
+
     Graph structure_;
     RandomNumberGenerator rng_;
-
-    using SpinPack = std::vector<std::uint64_t>;
-    using StateVectorPack = std::vector<SpinPack>;
 
     StateVectorPack population_;
 
@@ -27,10 +42,10 @@ protected:
 
 /** Carries out moves monte carlo sweeps of all replicas on the accelerator.
  */
-    void Sweep(int moves);
+    void SweepPopulation(int sweeps);
 /** Returns true if a move may be made that reduces the total energy.
  */
-    double Resample(double new_beta, double population_fraction);
+    // double Resample(double new_beta, double population_fraction);
 /** Returns new population size
  * Uses a logistic curve with parameters given in input file
  * This probably will be removed in the future with preference given 
@@ -47,5 +62,6 @@ public:
     FpgaPopulationAnnealing(Graph& structure, Config schedule);
 /** Run solver and return results.
  */
+    std::vector<Result> Run();
 };
 }
